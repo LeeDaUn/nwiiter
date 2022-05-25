1769cfaac8dd6fce81bd4bfb87bd23e151147ae6
LeeDaUn <dnwjd0559@gmail.com>
2022-05-25 21:19:48 +0900

    0525 commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
-
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(()=>{
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      if (user){
+      if (user) {
         setIsLoggedIn(user);
-      }else{
+        setUserObj(user);
+      } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
 
-  return(
+  return (
     <>
-      {init ? <AppRouter isLoggedIn = {isLoggedIn} /> : " initializing..."}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "initializing..."
+      )}
     </>
   );
 }
-
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,68 +1,87 @@
-import {dbService} from "fbase";
-import {useEffect,useState} from "react";
+import { dbService } from "fbase";
+import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
 
-const Home = ({userObj}) =>{
-    console.log(userObj);
-    const [nweet, setNweet] = useState("");
-    const [nweets, setNweets] = useState([]);
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState("");
 
-    // const getNweets = async ()=>{
-    //     const dbNweets = await dbService.collection("nweets").get();
-    //     dbNweets.forEach((document) =>{
-    //         const nweetObject = { ...document.data(), id: document.id };
-    //         setNweets((prev) => [nweetObject, ...prev])
-    //     });
-    // };
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
+  }, []);
 
-    useEffect(()=>{
-        dbService.collection("nweets").onSnapshot((snapshot)=>{
-            const newArray = snapshot.docs.map((document)=>({
-                id: document.id,
-                ...document.data(),
-            }));
-            setNweet(newArray);
-        });
-    }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
 
-    const onSubmit = async(event)=>{
-        event.preventDefault();
-        await dbService.collection("nweets").add({
-            text:nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-        });
-        setNweet("");
-    };
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
 
-    const onChange = (event) =>{
-        event.preventDefault();
-        const{
-            target: {value},
-        } = event;
-        setNweet(value);
-    };
+  const onFileChange = (event)=>{
+      const{
+          target:{files},
+      } = event;
+      const theFile = files[0];
+      const reader = new FileReader();
+      reader.onloadend = (finishedEvent) =>{
+          const{
+              currentTarget : {result},
+          } = finishedEvent;
+          setAttachment(result);
+      };
+      reader.readAsDataURL(theFile);
+  };
 
-    return (
-        <>
-            <form onSubmit={onSubmit}>
-                <input
-                value ={nweet}
-                onChange={onChange}
-                type="text"
-                placeholder="What's on your mind?"
-                maxLength={120}
-            />
-            <input type = "submit" value="Nweet" />
-            </form>
+  const onClearAttachment = () => setAttachment("");
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="file" accept="image/*" onChange={onFileChange} />
+        <input type="submit" value="Nweet" />
+        {attachment && (
             <div>
-                {nweets.map((nweet)=>(
-                    <div key = {nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
-                ))}
+                <img src ={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
             </div>
-        </>
-    );
+        )}
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
